8f2b9da73863172020fe45e3732283770a30972b
Carlos Eduardo Ibarra Manzano <carlosibarra999@outlook.com>
2025-05-01 16:20:41 -0500

    chore: implement initial server setup with MongoDB connection

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import colors from "colors"
+import server from "./server"
+
+const port = process.env.PORT || 4000
+
+server.listen(port, () => {
+    console.log(colors.cyan.italic("REST API working on port:"), colors.yellow.italic(`${port}`))
+})
