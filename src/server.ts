d68151cd9c532c9cc83be2f6c59249ce236d99b0
Carlos Eduardo Ibarra Manzano <carlosibarra999@outlook.com>
2025-05-01 22:33:29 -0500

    feat: add authentication routes and integrate with server

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from "express"
 import dotenv from "dotenv"
 import { connectDB } from "./config/db"
+import authRoutes from "./routes/authRoutes"
 
 dotenv.config()
-
 connectDB()
 
 const app = express()
+
+// Routes
+app.use('/api/auth', authRoutes)
 
 export default app
