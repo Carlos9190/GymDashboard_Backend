cc7c8f85c1c7101d77f83c81ceaffef75856c1de
Carlos Eduardo Ibarra Manzano <carlosibarra999@outlook.com>
2025-05-01 23:07:04 -0500

    feat: add CORS support and update dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
+import cors from "cors"
+import { corsConfig } from "./config/cors"
 import { connectDB } from "./config/db"
 import authRoutes from "./routes/authRoutes"
 
@@ -7,6 +9,7 @@ dotenv.config()
 connectDB()
 
 const app = express()
+app.use(cors(corsConfig))
 
 // Routes
 app.use('/api/auth', authRoutes)
