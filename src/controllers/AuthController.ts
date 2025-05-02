4918a14c19848eec48e0424031c99489dbad01ce
Carlos Eduardo Ibarra Manzano <carlosibarra999@outlook.com>
2025-05-02 17:44:05 -0500

    feat: implement user account creation and email confirmation functionality

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,68 @@
+import type { Request, Response } from "express"
+import User from "../models/User"
+import { hashPassword } from "../utils/auth"
+import Token from "../models/Token"
+import { generateToken } from "../utils/token"
+import { AuthEmail } from "../emails/AuthEmail"
+
+export class AuthController {
+
+    static createAccount = async (req: Request, res: Response) => {
+        try {
+            const { password, email } = req.body
+
+            // Prevent duplicates
+            const userExists = await User.findOne({ email })
+            if (userExists) {
+                const error = new Error('Already existing user')
+                res.status(409).json({ error: error.message })
+                return
+            }
+
+            // Create an user
+            const user = new User(req.body)
+
+            // Hash password
+            user.password = await hashPassword(password)
+
+            // Generate token
+            const token = new Token()
+            token.token = generateToken()
+            token.user = user.id
+
+            // Send Email
+            AuthEmail.sendConfirmationEmail({
+                email: user.email,
+                name: user.name,
+                token: token.token
+            })
+
+            await Promise.allSettled([user.save(), token.save()])
+            res.send('Account created, check your email to confirm it')
+        } catch (error) {
+            res.status(500).json({ error: 'There was an error' })
+        }
+    }
+
+    static confirmAccount = async (req: Request, res: Response) => {
+        try {
+            const { token } = req.body
+
+            const tokenExists = await Token.findOne({ token })
+
+            if (!tokenExists) {
+                const error = new Error('Invalid token')
+                res.status(404).json({ error: error.message })
+                return
+            }
+
+            const user = await User.findById(tokenExists.user)
+            user.confirmed = true
+
+            await Promise.allSettled([user.save(), tokenExists.deleteOne()])
+            res.send('Successfully confirmed account')
+        } catch (error) {
+            res.status(500).json({ error: 'There was an error' })
+        }
+    }
+}
