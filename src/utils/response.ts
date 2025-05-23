a06437212c372a70f440ffbaf96af5c221642e23
Carlos Eduardo Ibarra Manzano <carlosibarra999@outlook.com>
2025-05-23 10:19:10 -0500

    rename responses.ts file

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,9 +1,9 @@
 type ApiResponse<T> = {
-    message: string;
-    success: boolean;
-    data: T;
-};
+    message: string
+    success: boolean
+    data: T
+}
 
 export function createResponse<T>(message: string, success: boolean, data?: T): ApiResponse<T> {
-    return { message, success, data };
+    return { message, success, data }
 }
